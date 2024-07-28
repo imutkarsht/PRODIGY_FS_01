a05c976082fa87a9cb1791dcd5fb2049b23f0318
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-07-28 13:13:44 +0530

    bug fix

--- public/scripts/handleDeletePopup.js
+++ public/scripts/handleDeletePopup.js
@@ -1,19 +1,25 @@
-const delBtn = document.getElementById('delBtn')
-const delPopup = document.getElementById('delPopup')
-const cancelbtn = document.getElementById('cancelbtn')
+const delBtns = document.querySelectorAll('.delBtn');
+const delPopups = document.querySelectorAll('.delPopup');
+const cancelBtns = document.querySelectorAll('.cancelbtn');
 
-delBtn.addEventListener('click', () => {
-    delPopup.classList.remove('hidden')
-    delPopup.classList.add('animate-ping')
-    delPopup.classList.add('flex')
-    setTimeout(() => delPopup.classList.remove('animate-ping'),150)
-})
+delBtns.forEach((delBtn, index) => {
+    delBtn.addEventListener('click', () => {
+        const delPopup = delPopups[index];
+        delPopup.classList.remove('hidden');
+        delPopup.classList.add('animate-ping');
+        delPopup.classList.add('flex');
+        setTimeout(() => delPopup.classList.remove('animate-ping'), 150); 
+    });
+});
 
-cancelbtn.addEventListener('click', () => {
-    delPopup.classList.add('animate-ping')
-    setTimeout(() => {
-        delPopup.classList.remove('animate-ping')
-        delPopup.classList.remove('flex');
-        delPopup.classList.add('hidden');
-    },300)
-})
+cancelBtns.forEach((cancelBtn, index) => {
+    cancelBtn.addEventListener('click', () => {
+        const delPopup = delPopups[index];
+        delPopup.classList.add('animate-ping');
+        setTimeout(() => {
+            delPopup.classList.remove('animate-ping');
+            delPopup.classList.remove('flex');
+            delPopup.classList.add('hidden');
+        }, 150);
+    });
+});
